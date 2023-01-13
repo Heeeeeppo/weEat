01e1d3dbd431ffd659214726e6ce495f9b5145e7
Kevin <mqfang211@gmail.com>
2023-01-12 19:34:10 -0500

    vendor and customer dashboard

--- accounts/utils.py
+++ accounts/utils.py
@@ -18,7 +18,7 @@ def detectUser(user):
         return redirectUrl
 
 def send_verification_email(request, user, mail_subject, email_template):
-    from_eamil = settings.DEFAULT_FROM_EMAIL
+    from_email = settings.DEFAULT_FROM_EMAIL
     current_site = get_current_site(request)
     message = render_to_string(email_template, {
         'user': user,
@@ -27,5 +27,12 @@ def send_verification_email(request, user, mail_subject, email_template):
         'token': default_token_generator.make_token(user),  
     })
     to_email = user.email
-    mail = EmailMessage(mail_subject, message,from_eamil, to=[to_email])
+    mail = EmailMessage(mail_subject, message,from_email, to=[to_email])
+    mail.send()
+
+def send_notification(mail_subject, mail_template, context):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(mail_template, context)
+    to_email = context['user'].email
+    mail = EmailMessage(mail_subject, message,from_email, to=[to_email])
     mail.send()
